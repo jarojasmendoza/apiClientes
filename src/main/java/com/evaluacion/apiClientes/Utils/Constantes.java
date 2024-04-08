e041ec6bc19a369f6975171fc27f8247c89f44fa
José Rojas <jarojasm@canvia.com>
2024-04-08 13:36:38 -0500

    fix - mejoras

--- src/main/java/com/evaluacion/apiClientes/Utils/Constantes.java
+++ src/main/java/com/evaluacion/apiClientes/Utils/Constantes.java
@@ -1,6 +1,22 @@
 package com.evaluacion.apiClientes.Utils;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Constantes {
-    public static final String REGLA_CORREO = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-    public static final String REGLA_CLAVE = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+    public static String REGLA_CORREO;
+    @Value("${api.usuario.regla.correo}")
+    private String reglaCorreoValue;
+
+    public static  String REGLA_CLAVE;
+    @Value("${api.usuario.regla.clave}")
+    private String reglaClaveValue;
+    @PostConstruct
+    public void init() {
+        REGLA_CORREO = reglaCorreoValue;
+        REGLA_CLAVE = reglaClaveValue;
+    }
+
 }
